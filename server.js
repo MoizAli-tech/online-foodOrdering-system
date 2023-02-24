f4c20772fa4678d91ce8c53ea63e4e88aa6bebe5
moiz <amoiz.3149@gmail.com>
2023-02-24 23:13:20 +0500

    initial

---
+++ server.js
@@ -0,0 +1,24 @@
+const PORT = process.env.PORT || 5000;
+const express = require("express");
+const app = express();
+const expressLayout = require("express-ejs-layouts");
+const path = require("path");
+
+
+//middlewares 
+// app.use(expressLayout);
+app.use(express.static(path.join(__dirname,"dist")));
+
+  
+
+//view engine 
+app.set("views",path.join(__dirname,"./src/views"));
+app.set("view engine", "ejs");
+
+//home route
+app.get("/",(req,res)=>{
+    res.render("home");
+})
+
+
+app.listen(PORT);
