7abd5e497a338b0f5e9c0ecc455e9ccebf6bed43
moiz <amoiz.3149@gmail.com>
2023-02-25 00:47:26 +0500

    make homepage

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 const express = require("express");
 const app = express();
 const expressLayout = require("express-ejs-layouts");
